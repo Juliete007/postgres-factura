9a7f48aba7e5028005f580363a9178801f2a41cc
Juliete007 <julio.frances@gmail.com>
2023-09-20 18:41:58 +0200

    Update buscar.php

--- buscar.php
+++ buscar.php
@@ -4,76 +4,94 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="estilos.css">
-    <title>Resultados de la Búsqueda</title>
+    <title>Consulta de Tablas y Variables</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
-    <h1>Resultados de la Búsqueda</h1>
+    <h1>Consulta de Tablas y Variables</h1>
     <?php
-    // Verificar si se ha enviado el formulario
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Establecer una conexión a la base de datos PostgreSQL
-        $dbhost = "bnk6mxyzwg04dorrxsyk-postgresql.services.clever-cloud.com"; // Cambia esto al host de tu base de datos PostgreSQL
-        $dbname = "bnk6mxyzwg04dorrxsyk";
-        $dbuser = "ughuuywkcfgjimu2goew";
-        $dbpass = "yyXzddi5XIINcOBBpVCQxOCz2Wlvc0";
+    // Conexión a la base de datos PostgreSQL
+    $conn = pg_connect("host=bnk6mxyzwg04dorrxsyk-postgresql.services.clever-cloud.com dbname=bnk6mxyzwg04dorrxsyk user=ughuuywkcfgjimu2goew password=yyXzddi5XIINcOBBpVCQxOCz2Wlvc0");
 
-        try {
-            $conn = new PDO("pgsql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            die("Error de conexión: " . $e->getMessage());
-        }
-
-        // Obtener el Código de Población proporcionado por el usuario
-        $cod_pob = $_POST["cod_pob"];
-
-        // Preparar la consulta SQL para buscar clientes por Código de Población
-        $sql = "SELECT * FROM client WHERE cod_pob = :cod_pob";
-
-        try {
-            $stmt = $conn->prepare($sql);
-            $stmt->bindParam(':cod_pob', $cod_pob, PDO::PARAM_STR);
-            $stmt->execute();
-
-            // Verificar si se encontraron resultados
-            if ($stmt->rowCount() > 0) {
-                // Mostrar los resultados en una tabla
-                echo "<table border='1'>";
-                echo "<tr><th>Código del cliente</th><th>Nombre</th><th>Dirección</th><th>Código postal</th></tr>";
-                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                    echo "<tr><td>" . $row["cod_cli"] . "</td><td>" . $row["nom"] . "</td><td>" . $row["adreca"] . "</td><td>" . $row["cp"] . "</td></tr>";
-                }
-                echo "</table>";
-            } else {
-                echo "<h1>No se encontraron clientes en este Código de población</h1>";
-            }
-        } catch (PDOException $e) {
-            echo "Error: " . $e->getMessage();
-        }
-
-        // Cerrar la conexión a la base de datos
-        $conn = null;
-    } else {
-        // Si el formulario no se ha enviado, inicializa $cod_pob
-        $cod_pob = "";
+    if (!$conn) {
+        echo "<h1>Error: No se pudo conectar a la base de datos</h1>";
+        exit;
     }
+
+    // Obtener la tabla seleccionada del POST
+    $tablaSeleccionada = $_POST['tabla'];
+
+    // Consulta para obtener las columnas de la tabla
+    $query = "SELECT column_name, data_type 
+            FROM information_schema.columns 
+            WHERE table_name = '$tablaSeleccionada'";
+
+    $result = pg_query($conn, $query);
+
+    if (!$result) {
+        echo "<h1>Error en la consulta</h1>";
+        exit;
+    }
+
+    // Crear las opciones para el desplegable de variables
+    $options = '';
+    while ($row = pg_fetch_assoc($result)) {
+        $column_name = $row['column_name'];
+        $data_type = $row['data_type'];
+        $options .= "<option value='$column_name'>$column_name ($data_type)</option>";
+    }
+
+    // Liberar recursos y cerrar la conexión
+    pg_free_result($result);
+    pg_close($conn);
+
+    // Devolver las opciones como respuesta AJAX
+    echo $options;
     ?>
 
+
     <!-- Agrega un formulario para ingresar el Código de Población -->
-    <form method="POST" action="buscar.php">
-        <select>
+    <form action="consulta.php" method="post">
+        <label for="tabla">Selecciona una tabla:</label>
+        <select id="tabla" name="tabla">
             <option value="article">Artículo</option>
             <option value="categoria">Categoría</option>
             <option value="client">Cliente</option>
             <option value="factura">Factura</option>
-            <option value="linia_fac">Línia de la factura</option>
+            <option value="linia_fac">Línea de la factura</option>
             <option value="poble">Pueblo</option>
             <option value="provincia">Provincia</option>
             <option value="venedor">Vendedor</option>
         </select>
-        <label for="cod_pob">Código de la población:</label>
-        <input type="text" name="cod_pob" id="cod_pob" required>
+
+        <label for="variable">Selecciona una variable:</label>
+        <select id="variable" name="variable" disabled></select>
+
+        <label for="valor">Valor:</label>
+        <input type="text" id="valor" name="valor">
+
         <input type="submit" value="Buscar">
     </form>
+
+    <script>
+        $(document).ready(function () {
+            // Cuando cambie la selección de la tabla
+            $('#tabla').change(function () {
+                var tablaSeleccionada = $(this).val();
+
+                // Hacer una solicitud AJAX para obtener las variables de la tabla
+                $.ajax({
+                    url: 'get_variables.php',
+                    type: 'POST',
+                    data: { tabla: tablaSeleccionada },
+                    success: function (response) {
+                        // Llenar el desplegable de variables con la respuesta
+                        $('#variable').html(response);
+                        $('#variable').prop('disabled', false);
+                    }
+                });
+            });
+        });
+    </script>
 </body>
 </html>
